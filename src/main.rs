93c2273b4b9b4f4b6a754633614b90c7699f97f1
EdWorld <edwardutria12@gmail.com>
2022-10-06 14:18:12 -0400

    intial pipeline commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    println!("{}",hello_world());
+    println!("{}", bad_hello_world());
+}
+
+fn hello_world() -> &'static str {
+    let s = "Hello, world!";
+    return s;
+}
+
+fn bad_hello_world() -> &'static str{
+    let f = "Hello, fantasy";
+    return f;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_hello_world_success() {
+        assert_eq!(hello_world(), "Hello, world!")
+    }
+    #[test]
+    fn test_hello_world_bad() {
+        // Should fail this is to test how the pipeline react.
+        assert_eq!(bad_hello_world(), "Hello, world!")
+    }
+}
